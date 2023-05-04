e87948113690516388a65134f1bcb2a924977586
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-05-04 21:48:56 +0530

    Install react-router-dom  and apply Basic Rotings...

--- frontend_client/src/App.js
+++ frontend_client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <NavBar/>
+          <Route path="/" element={}/>
+          <Route path="/add" element={}/>
+          <Route path="/edit/:id" element={}/>
+        </Routes>
 
+      </BrowserRouter>
 
     </>
   );

89812760611a3c6254a914e49d41e4d12a330281
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-05-04 12:37:51 +0530

    Added mongoose to communicate through MongoDB and implement connection...

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,28 @@
-/**Import and  require express to app*/
+/**Import and require express , Mongoose to app*/
 const express =  require("express");
+//To communicate MongoDB through mongoose
+const mongoose =  require("mongoose");
 
 /**Invoked express to app*/
-const app = express();
+const APP = express();
 
 /** Declare Unique PORT */
-const port = 8000;
+const PORT = 8000;
+
+/** Declare Url variable */
+const DB_URL = 'mongodb+srv://ishara:ishara1234@item-crud-app.rope5xb.mongodb.net/item-crud-App?retryWrites=true&w=majority'
 
 /** The app is listened using Port */
-app.listen(port, ()=>{
-    console.log(`App is Running on ${port}`);
+APP.listen(PORT, ()=>{
+    console.log(`App is Running on ${PORT}`);
 });
 
+/** Create database Connection Using MongoDB */
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("DB Connected Successfully");
+}).catch((err)=>{
+    console.log("DB connection error "+err)
+})

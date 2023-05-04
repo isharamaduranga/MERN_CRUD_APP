d27698b7bf03193864acdfa436a81f9b4afa88f3
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-05-04 10:04:30 +0530

    Added comments...

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,13 @@
+/**Import and  require express to app*/
 const express =  require("express");
 
+/**Invoked express to app*/
 const app = express();
 
+/** Declare Unique PORT */
 const port = 8000;
 
+/** The app is listened using Port */
 app.listen(port, ()=>{
     console.log(`App is Running on ${port}`);
 });
